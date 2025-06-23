6c9c4572bfe90724fc7a78df64c8463d35523318
Aaron Barraza <aebs90@gmail.com>
2025-06-23 01:11:13 -0700

    Add the admin page

--- dashboard-notes.php
+++ dashboard-notes.php
@@ -14,3 +14,33 @@
  * Text Domain:       dashboard-notes
  * Domain Path:       /languages
  */
+
+namespace MIGHTYminnow\DashboardNotes;
+
+/**
+ * Register the admin page.
+ */
+function register_admin_page() {
+	add_menu_page(
+		esc_html__( 'Dashboard Notes', 'dashboard-notes' ),
+		esc_html__( 'Dashboard Notes', 'dashboard-notes' ),
+		'manage_options',
+		'dashboard-notes',
+		__NAMESPACE__ . '\display_admin_page',
+		'dashicons-admin-generic',
+		99
+	);
+}
+
+add_action( 'admin_menu', __NAMESPACE__ . '\register_admin_page' );
+
+/**
+ * Display the admin page.
+ */
+function display_admin_page() {
+	?>
+	<div class="wrap">
+		<h1><?php esc_html_e( 'Dashboard Notes', 'dashboard-notes' ); ?></h1>
+	</div>
+	<?php
+}
