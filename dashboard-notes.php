f60f936c8f6dc66e0f19571b1c8c9e365763e63d
Aaron Barraza <aebs90@gmail.com>
2025-10-08 23:05:47 -0700

    Remove Classic Widgets dependency

--- dashboard-notes.php
+++ dashboard-notes.php
@@ -13,7 +13,6 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       dashboard-notes
  * Domain Path:       /languages
- * Requires Plugins:  classic-widgets
  */
 
 /**
