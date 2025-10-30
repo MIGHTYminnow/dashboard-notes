7405b12a5b571c5170b63527950d2b294145c257
Aaron Barraza <aebs90@gmail.com>
2025-10-30 16:27:01 -0700

    Save new note from admin page form

--- includes/admin-page.php
+++ includes/admin-page.php
@@ -42,22 +42,19 @@ function display_admin_page() {
 	global $dashboard_notes;
 	?>
 	<div class="wrap">
-		<?php
-		$nonce = wp_create_nonce( 'dn-add' );
-		?>
-		<form id="dn-add">
+		<form id="dn-add" method="post" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>">
 			<div>
 				<label for="dn-add-title"><?php _e( 'Title', 'dashboard-notes' ); ?></label>
 				<input type="text" id="dn-add-title" name="title" required>
 			</div>
 
 			<div>
-				<label for="dn-add-content">Content</label>
+				<label for="dn-add-content"><?php _e( 'Content', 'dashboard-notes' ); ?></label>
 				<textarea id="dn-add-content" name="content" rows="6" required></textarea>
 			</div>
 
-			<input type="hidden" name="action" value="dn-add">
-			<input type="hidden" name="nonce" value="<?php echo esc_attr( $nonce ); ?>">
+			<input type="hidden" name="action" value="save_dashboard_note">
+			<?php wp_nonce_field( 'save_dashboard_note_nonce', 'save_dashboard_note_nonce_field' ); ?>
 
 			<div>
 				<button type="submit" id="dn-add-submit">Save</button>
@@ -96,57 +93,61 @@ function display_admin_page() {
 	<?php
 }
 
-/** Add Test Widget Instance */
-add_action( 'init', function() {
-	if ( isset( $_GET['debug'] ) ) {
-		$widget_type = 'custom_html'; // ID base of the widget (e.g. 'text', 'recent-posts', 'nav_menu', etc.)
-		$sidebar_id  = 'dashboard-notes'; // Sidebar ID (as registered in your code)
-		
-		// 1. Get all existing widget instances of that type
-		$all_widgets = get_option( 'widget_' . $widget_type, [] );
+add_action( 'admin_post_save_dashboard_note', __NAMESPACE__ . '\save_note' );
 
-		// 2. Create a new instance
-		$new_widget = [
-			'title'  => 'Programmatically Added Widget',
-			'content'   => '<p>This widget was added via code!</p>',
-		];
+function save_note() {
+	check_admin_referer( 'save_dashboard_note_nonce', 'save_dashboard_note_nonce_field' );
 
-		// 3. Append to the array and get the new instance key
-		$all_widgets[] = $new_widget;
-		end( $all_widgets );
-		$new_instance_id = key( $all_widgets );
+	$title = sanitize_text_field( $_POST['title'] ?? '' );
+	$content = sanitize_textarea_field( $_POST['content'] ?? '' );
 
-		// 4. Save back the updated widget options
-		update_option( 'widget_' . $widget_type, $all_widgets );
+	$widget_type = 'custom_html'; // ID base of the widget (e.g. 'text', 'recent-posts', 'nav_menu', etc.)
+	$sidebar_id  = 'dashboard-notes'; // Sidebar ID (as registered in your code)
+	
+	// 1. Get all existing widget instances of that type
+	$all_widgets = get_option( 'widget_' . $widget_type, [] );
 
-		// 5. Assign that widget instance to a sidebar
-		$sidebars_widgets = get_option( 'sidebars_widgets', [] );
+	// 2. Create a new instance
+	$new_widget = [
+		'title' => $title,
+		'content' => $content,
+	];
 
-		// Make sure the sidebar exists
-		if ( ! isset( $sidebars_widgets[ $sidebar_id ] ) ) {
-			$sidebars_widgets[ $sidebar_id ] = [];
-		}
+	// 3. Append to the array and get the new instance key
+	$all_widgets[] = $new_widget;
+	end( $all_widgets );
+	$new_instance_id = key( $all_widgets );
 
-		$sidebars_widgets[ $sidebar_id ][] = $widget_type . '-' . $new_instance_id;
+	// 4. Save back the updated widget options
+	update_option( 'widget_' . $widget_type, $all_widgets );
 
-		update_option( 'sidebars_widgets', $sidebars_widgets );
+	// 5. Assign that widget instance to a sidebar
+	$sidebars_widgets = get_option( 'sidebars_widgets', [] );
 
-
-		// 6. Add Dashboard Notes options
-		$options = get_option( 'dashboard_notes_options' );
-
-		$options[ $widget_type . '-' . $new_instance_id ] = array(
-			'style' => 'red',
-			'include-logo' => 1,
-			'logo-url' => '',
-			'incexc' => 'selected',
-			'url' => array(
-				'urls' => 'plugins.php',
-			),
-		);
-
-		update_option( 'dashboard_notes_options', $options );
-
-		exit;
+	// Make sure the sidebar exists
+	if ( ! isset( $sidebars_widgets[ $sidebar_id ] ) ) {
+		$sidebars_widgets[ $sidebar_id ] = [];
 	}
-} );
+
+	$sidebars_widgets[ $sidebar_id ][] = $widget_type . '-' . $new_instance_id;
+
+	update_option( 'sidebars_widgets', $sidebars_widgets );
+
+	// 6. Add Dashboard Notes options
+	$options = get_option( 'dashboard_notes_options' );
+
+	$options[ $widget_type . '-' . $new_instance_id ] = array(
+		'style' => 'red',
+		'include-logo' => 1,
+		'logo-url' => '',
+		'incexc' => 'selected',
+		'url' => array(
+			'urls' => 'plugins.php',
+		),
+	);
+
+	update_option( 'dashboard_notes_options', $options );
+
+	wp_redirect( admin_url( 'admin.php?page=dashboard-notes&saved=1' ) );
+	exit;
+}
